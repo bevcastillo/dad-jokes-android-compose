9216441f4d665edd13d46cf258e0446063bfad2d
Beverly Orofeo <beverlymaycastillo@gmail.com>
2024-06-30 15:31:09 +0800

    :sparkles: Add getJokes() implementation in MainScreenViewModel

--- app/src/main/java/com/example/dadjokesgenerator/screen/main/MainScreenViewModel.kt
+++ app/src/main/java/com/example/dadjokesgenerator/screen/main/MainScreenViewModel.kt
@@ -1,8 +1,38 @@
 package com.example.dadjokesgenerator.screen.main
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.lifecycle.HiltViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.dadjokesgenerator.screen.main.repository.JokesRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
 
-@HiltViewModel
-class MainScreenViewModel : ViewModel() {
+class MainScreenViewModel : ViewModel(), KoinComponent {
+    private val repository: JokesRepository by inject()
+    private val _state = MutableStateFlow(MainScreenState())
+    val state = _state.asStateFlow()
+
+    init {
+        getJokes()
+    }
+
+    fun onEvent(event: MainScreenEvent) {
+        when (event) {
+            is MainScreenEvent.OnNextClicked -> {
+                getJokes()
+            }
+        }
+    }
+
+    private fun getJokes() {
+        viewModelScope.launch {
+            repository.getJokes().onSuccess { response ->
+                _state.value = MainScreenState(joke = response.joke)
+            }.onFailure { e ->
+                e.printStackTrace()
+            }
+        }
+    }
 }
